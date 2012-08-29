71877999827a2d152503f0d7890494b040a500b2
Robert Nelson <robertcnelson@gmail.com>
2012-08-29 15:34:31 -0500

    intial commit
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+CORES=1
+if [ "x${ARCH}" == "xx86_64" ] || [ "x${ARCH}" == "xi686" ] ; then
+	CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+	let CORES=$CORES+1
+fi
+
+unset GIT_OPTS
+unset GIT_NOEDIT
+LC_ALL=C git help pull | grep -m 1 -e "--no-edit" &>/dev/null && GIT_NOEDIT=1
+
+if [ "${GIT_NOEDIT}" ] ; then
+	GIT_OPTS+="--no-edit"
+fi
+
+CCACHE=ccache
+
+config="at91_dt_defconfig"
+
+#Kernel/Build
+KERNEL_REL=3.6
+KERNEL_TAG=${KERNEL_REL}-rc3
+BUILD=v50
+
+#git branch
+#BRANCH=3.2.x
+
+BUILDREV=1.0
+DISTRO=cross
+DEBARCH=armel
