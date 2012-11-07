7a3f29383f2e985fb86636804920fabbfc2015ee
Robert Nelson <robertcnelson@gmail.com>
2012-11-07 16:09:40 -0600

    bump to v3.7-rc4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,8 +22,8 @@ config="at91_dt_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.7
-KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=armv5-x0.6
+KERNEL_TAG=${KERNEL_REL}-rc4
+BUILD=armv5-x0.7
 
 #git branch
 #BRANCH=3.2.x
