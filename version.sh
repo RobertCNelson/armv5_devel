a276fd17dae43a57f470a83898bece342f50b116
Robert Nelson <robertcnelson@gmail.com>
2012-12-17 15:20:47 -0600

    first pass for imx233 board
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -19,12 +19,12 @@ fi
 
 CCACHE=ccache
 
-config="imx_v4_v5_defconfig"
+config="mxs_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.7
-KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=imxv5-x0
+KERNEL_TAG=${KERNEL_REL}.1
+BUILD=imxv5-x0.1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="fa4c95bfdb85d568ae327d57aa33a4f55bab79c4"
