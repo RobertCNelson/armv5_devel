7b92238a5c236ed00bed5031aceb8fd2749b15ff
Robert Nelson <robertcnelson@gmail.com>
2013-01-18 10:04:04 -0600

    bump: v3.8-rc4: config disabled CONFIG_RTC_DRV_AT91RM9200 due to build error
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ config="at91_dt_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.8
-KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=armv5-x0.4
+KERNEL_TAG=${KERNEL_REL}-rc4
+BUILD=armv5-x0.5
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
