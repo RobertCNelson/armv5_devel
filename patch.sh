674463214d3d6be4dfccfef2be3185631886b1a4
Robert Nelson <robertcnelson@gmail.com>
2013-02-22 09:21:46 -0600

    merge to: https://github.com/RobertCNelson/linux/commit/2ef14f465b9e096531343f5b734cffc5f759f4a6
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -73,9 +73,9 @@ atmel_aria () {
 	${git} "${DIR}/patches/atmel_aria/0003-arm-at91-ariag25-add-leds-onewire.patch"
 	${git} "${DIR}/patches/atmel_aria/0004-rtc-serial-cleanup.patch"
 	${git} "${DIR}/patches/atmel_aria/0005-atmel-at91-ariag25-u-s-art-and-spi-cleanup.patch"
-	${git} "${DIR}/patches/atmel_aria/0006-ARM-at91-at91sam9x5-fix-usart3-pinctrl-name.patch"
+#	${git} "${DIR}/patches/atmel_aria/0006-ARM-at91-at91sam9x5-fix-usart3-pinctrl-name.patch"
 	${git} "${DIR}/patches/atmel_aria/0007-atmel-at91-ariag25-rtc-fixes.patch"
-	${git} "${DIR}/patches/atmel_aria/0008-ARM-at91-at91sam9x5.dtsi-usart3-TXD.patch"
+#	${git} "${DIR}/patches/atmel_aria/0008-ARM-at91-at91sam9x5.dtsi-usart3-TXD.patch"
 
 #	${git} "${DIR}/patches/atmel_aria/0006-at91-sam9x-enable-nand.patch"
 }
