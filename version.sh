4534e33ab2ac06ebe862392c001e80eae04d96c3
Robert Nelson <robertcnelson@gmail.com>
2013-03-01 09:13:50 -0600

    merge to: https://github.com/RobertCNelson/linux/commit/b0af9cd9aab60ceb17d3ebabb9fdf4ff0a99cf50
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,10 +24,10 @@ config="at91_dt_defconfig"
 #Kernel/Build
 KERNEL_REL=3.8
 KERNEL_TAG=${KERNEL_REL}
-BUILD=armv5-x0.16
+BUILD=armv5-x0.17
 
 #v3.X-rcX + upto SHA
-KERNEL_SHA="de1a2262b006220dae2561a299a6ea128c46f4fe"
+KERNEL_SHA="b0af9cd9aab60ceb17d3ebabb9fdf4ff0a99cf50"
 
 #git branch
 BRANCH="v3.9.x-at91"
