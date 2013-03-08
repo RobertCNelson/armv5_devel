5b50af09548c25d69af2b70c86d711c9699ed805
Robert Nelson <robertcnelson@gmail.com>
2013-03-08 09:56:55 -0600

    merge after the arm pull
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,10 +24,10 @@ config="at91_dt_defconfig"
 #Kernel/Build
 KERNEL_REL=3.9
 KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=armv5-x0.23
+BUILD=armv5-x0.24
 
 #v3.X-rcX + upto SHA
-#KERNEL_SHA="b0af9cd9aab60ceb17d3ebabb9fdf4ff0a99cf50"
+KERNEL_SHA="47b3bc907328db968bc9b43c41f48f8d1e140750"
 
 #git branch
 BRANCH="v3.9.x-at91"
