5499d21e49969214b1da30b85f5bf9e304f7be44
Robert Nelson <robertcnelson@gmail.com>
2013-03-25 17:04:48 -0500

    bump: v3.9-rc4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -21,8 +21,8 @@ config="mxs_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.9
-KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=imxv5-x0.2
+KERNEL_TAG=${KERNEL_REL}-rc4
+BUILD=imxv5-x0.3
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="fa4c95bfdb85d568ae327d57aa33a4f55bab79c4"
