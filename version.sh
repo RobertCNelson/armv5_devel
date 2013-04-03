109bffc4b54d40a0f9ffd6a03a26ea9352c2a912
Robert Nelson <robertcnelson@gmail.com>
2013-04-03 15:59:56 -0500

    config: enable CONFIG_MMC_UNSAFE_RESUME=y
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,7 +22,7 @@ config="mxs_defconfig"
 #Kernel/Build
 KERNEL_REL=3.9
 KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=imxv5-x0.4
+BUILD=imxv5-x0.5
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="fa4c95bfdb85d568ae327d57aa33a4f55bab79c4"
