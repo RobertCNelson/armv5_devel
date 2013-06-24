30dae1cf82b9c4f152cd6ddad72eaaa9a48dd4cf
Robert Nelson <robertcnelson@gmail.com>
2013-06-24 09:39:19 -0500

    bump: v3.10-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,8 +22,8 @@ linaro_toolchain="arm9_gcc_4_7"
 
 #Kernel/Build
 KERNEL_REL=3.10
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=armv5-x0.14
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=armv5-x0.15
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
