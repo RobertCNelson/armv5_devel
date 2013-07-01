84d16987c7aaa9be64ed8caf03d2539440b204a2
Robert Nelson <robertcnelson@gmail.com>
2013-07-01 09:21:39 -0500

    start of v3.11.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -23,13 +23,13 @@ linaro_toolchain="arm9_gcc_4_7"
 #Kernel/Build
 KERNEL_REL=3.10
 KERNEL_TAG=${KERNEL_REL}
-BUILD=armv5-x0.16
+BUILD=armv5-x0
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="v3.10.x-at91"
+BRANCH="v3.11.x-at91"
 
 BUILDREV=1.0
 DISTRO=cross
