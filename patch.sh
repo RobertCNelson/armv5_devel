5ad3196c3d3691208bf9dddc8c224c0b5c65f38b
Robert Nelson <robertcnelson@gmail.com>
2013-08-06 17:02:06 -0500

    add: rtc patch from mainline
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -66,8 +66,7 @@ atmel_mci () {
 atmel_fixes () {
 	echo "dir: atmel_fixes"
 	${git} "${DIR}/patches/atmel_fixes/0001-at91-merge-mega-patch.patch"
-#	${git} "${DIR}/patches/atmel_fixes/0002-net-macb-fix-ISR-clear-on-write-behavior-only-for-so.patch"
-#	${git} "${DIR}/patches/atmel_fixes/0003-v2-RTC-for-at91sam9x5-family.patch"
+	${git} "${DIR}/patches/atmel_fixes/0002-ARM-at91-at91sam9x5-RTC-is-not-compatible-with-at91r.patch"
 }
 
 ariag25_fixes () {
--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ linaro_toolchain="arm9_gcc_4_7"
 #Kernel/Build
 KERNEL_REL=3.10
 KERNEL_TAG=${KERNEL_REL}.5
-BUILD=armv5-x0.20
+BUILD=armv5-x0.21
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
