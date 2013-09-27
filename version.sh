ede235dd5348009628e31c386b72ed2404b3b179
Robert Nelson <robertcnelson@gmail.com>
2013-09-27 12:22:22 -0500

    kernel bump: v3.10.13
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ linaro_toolchain="arm9_gcc_4_7"
 
 #Kernel/Build
 KERNEL_REL=3.10
-KERNEL_TAG=${KERNEL_REL}.12
-BUILD=armv5-x0.28
+KERNEL_TAG=${KERNEL_REL}.13
+BUILD=armv5-x0.29
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
