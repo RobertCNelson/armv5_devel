37b542f681121a69b5bbff3f239af6dacedc7436
Robert Nelson <robertcnelson@gmail.com>
2013-12-03 10:09:31 -0600

    merge to: https://github.com/RobertCNelson/linux/commit/dea4f48a0a301b23c65af8e4fe8ccf360c272fbf
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,10 +27,10 @@ linaro_toolchain="arm9_gcc_4_7"
 #Kernel/Build
 KERNEL_REL=3.13
 KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=armv5-r4
+BUILD=armv5-r5
 
 #v3.X-rcX + upto SHA
-#KERNEL_SHA=""
+KERNEL_SHA="dea4f48a0a301b23c65af8e4fe8ccf360c272fbf"
 
 #git branch
 BRANCH="v3.13.x-at91"
