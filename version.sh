ec4ab78a73aee742ba00ba3e4c8794006f23e95d
Robert Nelson <robertcnelson@gmail.com>
2014-02-24 10:58:00 -0600

    kernel bump: v3.12.13
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,8 +28,8 @@ toolchain="arm9_gcc_4_7"
 
 #Kernel/Build
 KERNEL_REL=3.12
-KERNEL_TAG=${KERNEL_REL}.10
-BUILD=armv5-r18
+KERNEL_TAG=${KERNEL_REL}.13
+BUILD=armv5-r19
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
