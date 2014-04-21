835aed2313ad1c6ff51a4e83c58bd7e1d46cf735
Robert Nelson <robertcnelson@gmail.com>
2014-04-21 12:11:39 -0500

    kernel bump: v3.15-rc2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,8 +28,8 @@ toolchain="gcc_linaro_eabi_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.15
-KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=imxv5-r1
+KERNEL_TAG=${KERNEL_REL}-rc2
+BUILD=imxv5-r2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
