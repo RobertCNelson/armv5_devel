611ac1f107a36923992bfcdcf5183534f3c074c5
Robert Nelson <robertcnelson@gmail.com>
2014-05-12 14:15:10 -0500

    kernel bump: v3.10.39
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,8 +27,8 @@ toolchain="gcc_linaro_eabi_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.10
-KERNEL_TAG=${KERNEL_REL}.37
-BUILD=armv5-r50
+KERNEL_TAG=${KERNEL_REL}.39
+BUILD=armv5-r51
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
