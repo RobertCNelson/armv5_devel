799d08017be7e1739a3760393290ce98b72915ef
Robert Nelson <robertcnelson@gmail.com>
2014-05-12 15:30:50 -0500

    kernel bump: v3.13.11
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,8 +27,8 @@ toolchain="gcc_linaro_eabi_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.13
-KERNEL_TAG=${KERNEL_REL}.10
-BUILD=imxv5-r10
+KERNEL_TAG=${KERNEL_REL}.11
+BUILD=imxv5-r11
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
