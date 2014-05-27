e6e8b52430c233163a33c0a248dd4d60cb7d8860
Robert Nelson <robertcnelson@gmail.com>
2014-05-27 11:24:56 -0500

    kernel bump: v3.15-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_eabi_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.15
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=armv5-r4
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=armv5-r5
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
