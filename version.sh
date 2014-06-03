c545076dd47ed43ddbb238039b5af551c700fb4d
Robert Nelson <robertcnelson@gmail.com>
2014-06-03 10:42:01 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -19,11 +19,12 @@ fi
 
 config="at91_dt_defconfig"
 
-#toolchain="gcc_arm_embedded_4_7"
 toolchain="gcc_linaro_eabi_4_8"
+#toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
+#toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=3.11
