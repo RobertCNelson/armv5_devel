953ea8d37c860f12f2c8d1d705be8f29f5880091
Robert Nelson <robertcnelson@gmail.com>
2015-01-16 11:36:31 -0600

    kernel bump: v3.19-rc4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_eabi_4_9"
 
 #Kernel/Build
 KERNEL_REL=3.19
-KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=armv5-r1
+KERNEL_TAG=${KERNEL_REL}-rc4
+BUILD=armv5-r2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
