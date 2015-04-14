186848534585db0d8b1bac5f0792008958df4f6a
Robert Nelson <robertcnelson@gmail.com>
2015-04-14 11:55:55 -0500

    kernel bump: v4.0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -19,16 +19,16 @@ toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
-KERNEL_REL=3.19
-KERNEL_TAG=${KERNEL_REL}.4
-BUILD=imxv5-r2
+KERNEL_REL=4.0
+KERNEL_TAG=${KERNEL_REL}
+BUILD=imxv5-r0
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="v3.19.x-imxv5"
+BRANCH="v4.0.x-imxv5"
 
 DISTRO=cross
 DEBARCH=armel
