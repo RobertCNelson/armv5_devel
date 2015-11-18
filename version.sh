01d3333bd8047a50c7fceee0e8a9d3615f26fade
Robert Nelson <robertcnelson@gmail.com>
2015-11-18 16:21:41 -0600

    kernel bump: v4.3
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -7,8 +7,8 @@ config="at91_dt_defconfig"
 #arm
 KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
-toolchain="gcc_linaro_eabi_4_9"
-#toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_eabi_4_9"
+toolchain="gcc_linaro_eabi_5"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
@@ -19,9 +19,9 @@ toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_aarch64_gnu_5"
 
 #Kernel/Build
-KERNEL_REL=4.2
-KERNEL_TAG=${KERNEL_REL}.6
-BUILD=armv5-r1
+KERNEL_REL=4.3
+KERNEL_TAG=${KERNEL_REL}
+BUILD=armv5-r0
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
@@ -29,7 +29,7 @@ kernel_rt=".X-rtY"
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="v4.2.x"
+BRANCH="v4.3.x"
 
 DISTRO=cross
 DEBARCH=armel
