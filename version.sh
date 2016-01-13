f6b98439381e9bc810cc880629dbb8f8c2de9c20
Robert Nelson <robertcnelson@gmail.com>
2016-01-13 12:39:39 -0600

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,19 +4,27 @@ ARCH=$(uname -m)
 
 config="mxs_defconfig"
 
+#arm
+KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 #toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_5"
+#arm64
+#KERNEL_ARCH=arm64
+#toolchain="gcc_linaro_aarch64_gnu_5"
 
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.6
 BUILD=imxv5-r2
+kernel_rt=".X-rtY"
 
-#v3.X-rcX + upto SHA
+#v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
