04ce27d7341718adca40a4c20459bfbe70c47683
Robert Nelson <robertcnelson@gmail.com>
2016-03-15 12:03:19 -0500

    kernel bump: v4.1.19
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,8 +24,8 @@ toolchain="gcc_linaro_eabi_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.1
-KERNEL_TAG=${KERNEL_REL}.15
-BUILD=${build_prefix}4
+KERNEL_TAG=${KERNEL_REL}.19
+BUILD=${build_prefix}5
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
