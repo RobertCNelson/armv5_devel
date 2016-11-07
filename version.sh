f9f136253bddbf02e91fc7aaa2444bbc497e0770
Robert Nelson <robertcnelson@gmail.com>
2016-11-07 13:36:01 -0600

    kernel bump: v4.1.35
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,8 +27,8 @@ toolchain="gcc_linaro_eabi_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.1
-KERNEL_TAG=${KERNEL_REL}.26
-BUILD=${build_prefix}6
+KERNEL_TAG=${KERNEL_REL}.35
+BUILD=${build_prefix}7
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
