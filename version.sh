6ff61538755fe95641d91dbbcacb2bc00583f395
Robert Nelson <robertcnelson@gmail.com>
2016-11-07 14:38:00 -0600

    kernel bump: v4.8.6
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -12,8 +12,8 @@ branch_postfix=".x-imxv5"
 KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
-toolchain="gcc_linaro_eabi_5"
-#toolchain="gcc_linaro_eabi_6"
+#toolchain="gcc_linaro_eabi_5"
+toolchain="gcc_linaro_eabi_6"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
@@ -26,9 +26,9 @@ toolchain="gcc_linaro_eabi_5"
 #toolchain="gcc_linaro_aarch64_gnu_6"
 
 #Kernel/Build
-KERNEL_REL=4.7
-KERNEL_TAG=${KERNEL_REL}.10
-BUILD=${build_prefix}1
+KERNEL_REL=4.8
+KERNEL_TAG=${KERNEL_REL}.6
+BUILD=${build_prefix}0
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
