9590c2bd933c87835cc8e7c0af96d0f29d90b2a2
Robert Nelson <robertcnelson@gmail.com>
2016-11-07 12:38:41 -0600

    kernel bump: v4.7.10
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,8 +27,8 @@ toolchain="gcc_linaro_eabi_5"
 
 #Kernel/Build
 KERNEL_REL=4.7
-KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=${build_prefix}0
+KERNEL_TAG=${KERNEL_REL}.10
+BUILD=${build_prefix}1
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
