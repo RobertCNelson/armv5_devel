f602821a3c9a29921316b70911970ed66f0bdef2
Robert Nelson <robertcnelson@gmail.com>
2017-10-10 12:27:04 -0500

    kernel bump: v4.12.14
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,10 +30,10 @@ toolchain="gcc_linaro_eabi_6"
 
 #Kernel
 KERNEL_REL=4.12
-KERNEL_TAG=${KERNEL_REL}-rc2
+KERNEL_TAG=${KERNEL_REL}.14
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}0
+BUILD=${build_prefix}1
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
