f3e509927c8fba69cc190d5700b3ad019d465e59
Robert Nelson <robertcnelson@gmail.com>
2017-10-10 12:48:50 -0500

    kernel bump: v4.14-rc4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,8 +13,8 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_eabi_5"
-toolchain="gcc_linaro_eabi_6"
-#toolchain="gcc_linaro_eabi_7"
+#toolchain="gcc_linaro_eabi_6"
+toolchain="gcc_linaro_eabi_7"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
@@ -29,8 +29,8 @@ toolchain="gcc_linaro_eabi_6"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 
 #Kernel
-KERNEL_REL=4.13
-KERNEL_TAG=${KERNEL_REL}.5
+KERNEL_REL=4.14
+KERNEL_TAG=${KERNEL_REL}-rc4
 kernel_rt=".X-rtY"
 #Kernel Build
 BUILD=${build_prefix}0
