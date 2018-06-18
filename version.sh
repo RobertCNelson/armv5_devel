bb0045ed53e112fbe7ecef6131f66c692e147575
Robert Nelson <robertcnelson@gmail.com>
2018-06-18 10:38:07 -0500

    kernel bump: v4.4.138
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,10 +30,10 @@ toolchain="gcc_linaro_eabi_5"
 
 #Kernel
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.91
+KERNEL_TAG=${KERNEL_REL}.138
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}9
+BUILD=${build_prefix}10
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
