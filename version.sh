d197afba7327b6adf3e3b7cc0b89a36f7951314b
Robert Nelson <robertcnelson@gmail.com>
2019-09-17 11:30:03 -0500

    kernel bump: v4.14.144
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=4.14
-KERNEL_TAG=${KERNEL_REL}.93
+KERNEL_TAG=${KERNEL_REL}.144
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}2
+BUILD=${build_prefix}3
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
