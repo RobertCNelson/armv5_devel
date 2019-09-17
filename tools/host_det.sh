0f6df43f0f4a567eec446d6dda688c68c4cfdf1d
Robert Nelson <robertcnelson@gmail.com>
2019-09-17 14:13:31 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -39,7 +39,7 @@ check_rpm () {
 }
 
 redhat_reqs () {
-	pkgtool="yum"
+	pkgtool="dnf"
 
 	#https://fedoraproject.org/wiki/Releases
 	unset rpm_pkgs
@@ -53,30 +53,37 @@ redhat_reqs () {
 	check_rpm
 	pkg="wget"
 	check_rpm
+	pkg="fakeroot"
+	check_rpm
+	pkg="xz"
+	check_rpm
+	pkg="lzop"
+	check_rpm
+	pkg="bison"
+	check_rpm
+	pkg="flex"
+	check_rpm
+	pkg="uboot-tools"
+	check_rpm
+	pkg="openssl-devel"
+	check_rpm
 
 	arch=$(uname -m)
 	if [ "x${arch}" = "xx86_64" ] ; then
 		pkg="ncurses-devel.x86_64"
 		check_rpm
+		pkg="libmpc-devel.x86_64"
+		check_rpm
 		if [ "x${ignore_32bit}" = "xfalse" ] ; then
 			pkg="ncurses-devel.i686"
+			check_rpm
+			pkg="libmpc-devel.i686"
 			check_rpm
 			pkg="libstdc++.i686"
 			check_rpm
 			pkg="zlib.i686"
 			check_rpm
 		fi
-	fi
-
-	if [ "$(which lsb_release)" ] ; then
-		rpm_distro=$(lsb_release -rs)
-		echo "RPM distro version: [${rpm_distro}]"
-
-		case "${rpm_distro}" in
-		22|23|24|25)
-			pkgtool="dnf"
-			;;
-		esac
 	fi
 
 	if [ "${rpm_pkgs}" ] ; then
@@ -203,6 +210,20 @@ debian_regs () {
 			#Codename:       testing
 			if [ "x${deb_lsb_ds}" = "xSolydXK" ] ; then
 				deb_distro="jessie"
+			fi
+		fi
+
+		if [ "x${deb_distro}" = "xunstable" ] ; then
+			echo "+ Warning: [lsb_release -cs] just returned [unstable], so now testing [lsb_release -is] instead..."
+			deb_lsb_is=$(lsb_release -is | awk '{print $1}')
+
+			#lsb_release -a
+			#Distributor ID: Deepin
+			#Description:    Deepin 15.9.2
+			#Release:        15.9.2
+			#Codename:       unstable
+			if [ "x${deb_lsb_is}" = "xDeepin" ] ; then
+				deb_distro="stretch"
 			fi
 		fi
 
@@ -379,14 +400,15 @@ debian_regs () {
 			#https://blog.linuxmint.com/?p=3671
 			deb_distro="bionic"
 			;;
+		tina)
+			#19.2
+			#https://blog.linuxmint.com/?p=3736
+			deb_distro="bionic"
+			;;
 		esac
 
 		#Future Debian Code names:
 		case "${deb_distro}" in
-		bullseye)
-			#11 bullseye: https://wiki.debian.org/DebianBullseye
-			deb_distro="sid"
-			;;
 		bookworm)
 			#12 bookworm:
 			deb_distro="sid"
@@ -396,11 +418,12 @@ debian_regs () {
 		#https://wiki.ubuntu.com/Releases
 		unset error_unknown_deb_distro
 		case "${deb_distro}" in
-		jessie|stretch|buster|sid)
+		jessie|stretch|buster|bullseye|sid)
 			#https://wiki.debian.org/LTS
 			#8 jessie: https://wiki.debian.org/DebianJessie
 			#9 stretch: https://wiki.debian.org/DebianStretch
 			#10 buster: https://wiki.debian.org/DebianBuster
+			#11 bullseye: https://wiki.debian.org/DebianBullseye
 			unset warn_eol_distro
 			;;
 		squeeze|wheezy)
@@ -410,9 +433,11 @@ debian_regs () {
 			warn_eol_distro=1
 			stop_pkg_search=1
 			;;
-		bionic|cosmic)
+		bionic|cosmic|disco|eoan)
 			#18.04 bionic: (EOL: April 2023) lts: bionic -> xyz
-			#18.10 cosmic: (EOL: )
+			#18.10 cosmic: (EOL: July 2019)
+			#19.04 disco: (EOL: )
+			#19.10 eoan: (EOL: )
 			unset warn_eol_distro
 			;;
 		yakkety|zesty|artful)
@@ -426,18 +451,7 @@ debian_regs () {
 			#16.04 xenial: (EOL: April 2021) lts: xenial -> bionic
 			unset warn_eol_distro
 			;;
-		utopic|vivid|wily)
-			#14.10 utopic: (EOL: July 23, 2015)
-			#15.04 vivid: (EOL: February 4, 2016)
-			#15.10 wily: (EOL: July 28, 2016)
-			warn_eol_distro=1
-			stop_pkg_search=1
-			;;
-		trusty)
-			#14.04 trusty: (EOL: April 2019) lts: trusty -> xenial
-			unset warn_eol_distro
-			;;
-		hardy|lucid|maverick|natty|oneiric|precise|quantal|raring|saucy)
+		hardy|lucid|maverick|natty|oneiric|precise|quantal|raring|saucy|trusty|utopic|vivid|wily)
 			#8.04 hardy: (EOL: May 2013) lts: hardy -> lucid
 			#10.04 lucid: (EOL: April 2015) lts: lucid -> precise
 			#10.10 maverick: (EOL: April 10, 2012)
@@ -447,6 +461,10 @@ debian_regs () {
 			#12.10 quantal: (EOL: May 16, 2014)
 			#13.04 raring: (EOL: January 27, 2014)
 			#13.10 saucy: (EOL: July 17, 2014)
+			#14.04 trusty: (EOL: April 25, 2019) lts: trusty -> xenial
+			#14.10 utopic: (EOL: July 23, 2015)
+			#15.04 vivid: (EOL: February 4, 2016)
+			#15.10 wily: (EOL: July 28, 2016)
 			warn_eol_distro=1
 			stop_pkg_search=1
 			;;
@@ -605,6 +623,28 @@ elif [ "${git_major}" -eq "${compare_major}" ] ; then
 			build_git="true"
 		fi
 	fi
+fi
+
+echo "-----------------------------"
+unset NEEDS_COMMAND
+check_for_command () {
+	if ! which "$1" >/dev/null 2>&1 ; then
+		echo "You're missing command $1"
+		NEEDS_COMMAND=1
+	else
+		version=$(LC_ALL=C $1 $2 | head -n 1)
+		echo "$1: $version"
+	fi
+}
+
+unset NEEDS_COMMAND
+check_for_command cpio --version
+check_for_command lzop --version
+
+if [ "${NEEDS_COMMAND}" ] ; then
+	echo "Please install missing commands"
+	echo "-----------------------------"
+	exit 2
 fi
 
 case "$BUILD_HOST" in
