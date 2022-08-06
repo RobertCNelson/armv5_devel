5b72f84fd2fecc2950f9e98e6ab197064a04cdfc
Robert Nelson <robertcnelson@gmail.com>
2022-08-05 19:17:47 -0500

    kernel bump: v5.15.55
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ build_prefix="-imxv5-r"
 branch_prefix="v"
 branch_postfix=".x-imxv5"
 
-#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v5.14-rc1
+#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v5.15-rc1
 #arm
 KERNEL_ARCH=arm
 DEBARCH=armel
@@ -16,8 +16,8 @@ DEBARCH=armel
 #toolchain="gcc_7_arm"
 #toolchain="gcc_8_arm"
 #toolchain="gcc_9_arm"
-toolchain="gcc_10_arm"
-#toolchain="gcc_11_arm"
+#toolchain="gcc_10_arm"
+toolchain="gcc_11_arm"
 #toolchain="gcc_12_arm"
 #arm64
 #KERNEL_ARCH=arm64
@@ -40,9 +40,9 @@ toolchain="gcc_10_arm"
 #toolchain="gcc_12_riscv64"
 
 #Kernel
-KERNEL_REL=5.14
-KERNEL_TAG=${KERNEL_REL}.21
-kernel_rt=".2-rt21"
+KERNEL_REL=5.15
+KERNEL_TAG=${KERNEL_REL}.55
+kernel_rt=".55-rt48"
 #Kernel Build
 BUILD=${build_prefix}1
 
